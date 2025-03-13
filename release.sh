d162b3278a8f22c412b5b43ed420b530dae5864c
lrobot <lrobot.qq@gmail.com>
2025-03-13 09:02:45 +0000

    info

--- release.sh
+++ release.sh
@@ -4,7 +4,10 @@ IMAGE_NAME="code-encryptor"
 CONTAINER_NAME="code-encryptor-builder"
 Y4_VERSION="0.4"
 
-podman build .
-podman push `podman build -q .` docker.io/lrobot/code-encryptor:$Y4_VERSION
+echodo() { echo _run_cmd:"$@"; $@; }
+
+podman build . &&
+echodo podman push --retry=10 `podman build -q .` docker.io/lrobot/code-encryptor:$Y4_VERSION && {
 echo "podman run --rm -it lrobot/code-encryptor:0.4 java -jar /app.jar"
+}
 
